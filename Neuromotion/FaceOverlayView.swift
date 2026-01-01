836f5f56fb13d18b1caa80c564694f959d16ea54
Hrish <hrishdave@yahoo.com>
2026-01-01 13:57:10 -0500

    V2, upgraded UI with coloured comparison

---
+++ Neuromotion/FaceOverlayView.swift
@@ -0,0 +1,8 @@
+//
+//  FaceOverlayView.swift
+//  Neuromotion
+//
+//  Created by Hrish Dave on 2025-12-30.
+//
+
+import Foundation
---
+++ Neuromotion/SymmetryBarView.swift
@@ -0,0 +1,8 @@
+//
+//  SymmetryBarView.swift
+//  Neuromotion
+//
+//  Created by Hrish Dave on 2025-12-30.
+//
+
+import Foundation
